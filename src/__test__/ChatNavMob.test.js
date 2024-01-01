0076ec3b3992341a4416cd5761848a6429480254
Shahmeer-Sheraz <103183988+Shahmeer-Sheraz@users.noreply.github.com>
2024-01-01 23:40:10 +0500

    Test cases for ChatNav Added

---
+++ src/__test__/ChatNavMob.test.js
@@ -0,0 +1,71 @@
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom"; // Updated import
+import { BrowserRouter as Router } from "react-router-dom";
+
+
+import ChatNavMob from "../Components/ChatNavMob";
+import Subscribe from "../Components/Subscribe";
+
+describe("Subscribe Component", () => {
+  it("renders subscribe component", () => {
+    render(
+      <Router>
+        <Subscribe />
+      </Router>
+    );
+
+    // Check if the main heading is rendered
+    expect(screen.getByText("Join the waitlist")).toBeInTheDocument();
+
+    // Check if the subheading is rendered
+    expect(
+      screen.getByText(
+        "Join our waitlist and embark on a transformative communication journey."
+      )
+    ).toBeInTheDocument();
+
+    // Check if the email input field is rendered
+    expect(screen.getByPlaceholderText("Enter your e-mail.")).toBeInTheDocument();
+
+    // Check if the subscribe button is rendered
+    expect(screen.getByText("Subscribe")).toBeInTheDocument();
+  });
+
+  it("handles email input correctly", () => {
+    render(
+      <Router>
+        <Subscribe />
+      </Router>
+    );
+
+    const emailInput = screen.getByPlaceholderText("Enter your e-mail.");
+
+    // Type a valid email address
+    fireEvent.change(emailInput, { target: { value: "test@example.com" } });
+
+    // Check if the input value is updated
+    expect(emailInput.value).toBe("test@example.com");
+  });
+
+  it("subscribes on button click", () => {
+    render(
+      <Router>
+        <Subscribe />
+      </Router>
+    );
+
+    
+    const emailInput = screen.getByPlaceholderText("Enter your e-mail.");
+    const subscribeButton = screen.getByText("Subscribe");
+
+    // Type a valid email address
+    fireEvent.change(emailInput, { target: { value: "test@example.com" } });
+
+    // Click the subscribe button
+    fireEvent.click(subscribeButton);
+
+    // You may want to add assertions related to the subscription logic if applicable
+    // For example, check if a subscription request is made to the server
+  });
+});
