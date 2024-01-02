6df606a4073e34a29393a34be636ab2ff777de85
Vishal sagar <108807167+visha1Sagar@users.noreply.github.com>
2024-01-02 12:51:33 +0500

    Add Comit for TeamHeading

--- src/Components/TermsHeading.js
+++ src/Components/TermsHeading.js
@@ -1,6 +1,18 @@
 import React, { useState } from "react";
 import { FaHashtag } from "react-icons/fa";
 
+/**
+ * TermsHeading Component
+ *
+ * Displays a heading with an optional link for quick navigation.
+ *
+ * @component
+ * @param {Object} props - The component props.
+ * @param {string} props.heading - The main heading text.
+ * @param {string} props.text - The optional additional text.
+ * @returns {JSX.Element} - The rendered TermsHeading component.
+ */
+
 const TermsHeading = ({ heading, text }) => {
   const [showLink, setShowLink] = useState(false);
 
